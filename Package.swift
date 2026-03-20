dca8a47b20215ceeeea256a0495787e3bd1723dc
euphoriacyberware-ai <brian.cantin@gmail.com>
2026-03-20 17:56:01 -0400

    Adding fpzip library directly to client to fix upstream versioning errors.

--- Package.swift
+++ Package.swift
@@ -17,21 +17,47 @@ let package = Package(
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.23.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.27.0"),
         .package(url: "https://github.com/google/flatbuffers.git", exact: "25.9.23"),
-        .package(url: "https://github.com/weiyanlin117/swift-fpzip-support.git", revision: "0ec6d4668c9c83bc3da0f8b2d6dfc46da0b98609"),
     ],
     targets: [
+        .target(
+            name: "CFpzip",
+            path: "Sources/CFpzip",
+            exclude: [
+                "LICENSE",
+                "src/CMakeLists.txt",
+                "src/Makefile",
+                "src/fpe.inl",
+                "src/pccodec.inl",
+                "src/pcdecoder.inl",
+                "src/pcencoder.inl",
+                "src/pcmap.inl",
+                "src/rcdecoder.inl",
+                "src/rcencoder.inl",
+                "src/rcqsmodel.inl",
+            ],
+            sources: ["src"],
+            publicHeadersPath: "include",
+            cxxSettings: [
+                .define("FPZIP_FP", to: "FPZIP_FP_FAST"),
+                .define("FPZIP_BLOCK_SIZE", to: "0x1000"),
+                .headerSearchPath("src"),
+                .headerSearchPath("include"),
+                .unsafeFlags(["-std=c++98", "-fPIC"]),
+            ]
+        ),
         .target(
             name: "DrawThingsClient",
             dependencies: [
                 .product(name: "GRPC", package: "grpc-swift"),
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
                 .product(name: "FlatBuffers", package: "flatbuffers"),
-                .product(name: "C_fpzip", package: "swift-fpzip-support"),
+                "CFpzip",
             ]
         ),
         .testTarget(
             name: "DrawThingsClientTests",
             dependencies: ["DrawThingsClient"]
         ),
-    ]
+    ],
+    cxxLanguageStandard: .cxx11
 )
